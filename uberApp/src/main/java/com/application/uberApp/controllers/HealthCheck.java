adea51880164c7e82df588064c014772fff1d4db
Hritvik Agarwal <agarwal.hritvik18@gmail.com>
2024-10-08 17:59:48 +0530

    dfe

--- uberApp/src/main/java/com/application/uberApp/controllers/HealthCheck.java
+++ uberApp/src/main/java/com/application/uberApp/controllers/HealthCheck.java
@@ -1,2 +1,13 @@
-package com.application.uberApp.controllers;public class HealthCheck {
+package com.application.uberApp.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+    @GetMapping
+    public ResponseEntity<String> healthCheckController(){
+        return ResponseEntity.ok("OK");
+    }
 }
